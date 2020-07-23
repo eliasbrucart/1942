ddf84c92193dc17815789c8fc6ce80eee135271b
Elias <eliasbrucart@hardgames.com.ar>
2020-07-23 04:40:42 -0300

    Check if bullet is out of screen

--- scripts/bullet.js
+++ scripts/bullet.js
@@ -9,10 +9,13 @@ var bullet = {
 
 	update: function(delta){
 		this.y -= this.speed;
+		if(this.y + this.height < 0){
+			this.visible = false;
+		}
 	},
 
 	draw: function(contexto){
-		if(this.speed){
+		if(this.visible){
 			contexto.drawImage(spriteSheet, 140, 82, 17, 12, this.x, this.y, this.width, this.height);
 		}
 	}
