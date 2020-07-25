5d76fa661ffe6a492eaf1eced8b27c5251582e18
Elias <eliasbrucart@hardgames.com.ar>
2020-07-25 05:12:12 -0300

    Added impact function in enemy

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -2,11 +2,21 @@ var enemy = {
 	x:(Math.random() * 400) + 40,
 	y:1,
 
-	update:function(){
-		this.y++;
+	impact: function(){
+		this.y = -20;
+		this.x = (Math.random() * 400) + 40;
 	},
 
-	draw:function(contexto){
+	update: function(){
+		this.y++;
+
+		if(this.y > 480){
+			this.y = -20;
+			this.x = (Math.random() * 400) + 40;
+		}
+	},
+
+	draw: function(contexto){
 		contexto.drawImage(spriteSheet, 5, 200, 14, 15, this.x, this.y, 14, 15);
 	}
 };
