6168fc68244f7b88d28d9a8b97b2869dedd5aea3
Elias <eliasbrucart@hardgames.com.ar>
2020-07-25 05:09:40 -0300

    Bullet - enemy collision logic

--- scripts/bullet.js
+++ scripts/bullet.js
@@ -1,6 +1,6 @@
 var bullet = {
-	x: 0,
-	y: 0,
+	x: 320,
+	y: 240,
 	width: 17,
 	height: 12,
 	name: 'bullet',
@@ -9,9 +9,38 @@ var bullet = {
 
 	update: function(delta){
 		this.y -= this.speed;
+
 		if(this.y + this.height < 0){
 			this.visible = false;
 		}
+		for(var i = 0; i < framework.gameObjects.length; i++){
+			if(framework.gameObjects[i].name == 'enemy'){
+				if(this.collision(framework.gameObjects[i])){
+					this.visible = false;
+					framework.gameObjects[i].impact();
+					break;
+				}
+			}
+		}
+	},
+
+	collision: function(enemy){
+		if(this.x + this.width < enemy.x){
+			return false;
+		}
+
+		if(this.y + this.height < enemy.y){
+			return false;
+		}
+
+		if(this.x > enemy.x + enemy.width){
+			return false;
+		}
+
+		if(this.y > enemy.y + enemy.height){
+			return false;
+		}
+		return true;
 	},
 
 	draw: function(contexto){
