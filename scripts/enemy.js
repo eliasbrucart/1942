08547ef708f30346eb65c5527ec4a3a37de3db94
Elias <eliasbrucart@hardgames.com.ar>
2020-07-26 04:48:31 -0300

    New functions for animations in enemy

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -1,10 +1,45 @@
 var enemy = {
 	x:(Math.random() * 400) + 40,
 	y:1,
+	explosionIndex: 0,
+	explosionAccumulator: 0,
 
 	impact: function(){
-		this.y = -20;
-		this.x = (Math.random() * 400) + 40;
+		this.update = this.updateDeath;
+		this.draw = this.drawDeath;
+	},
+
+	updateDeath: function(delta){
+		this.explosionAccumulator += delta;
+		if(this.explosionAccumulator > 0.1){
+			this.explosionAccumulator = 0;
+			this.explosionIndex++;
+
+			if(this.explosionIndex >= 3){
+				this.explosionIndex = 0;
+				this.y = -20;
+				this.x = (Math.random() * 400) + 40;
+				this.update = this.updateNormal;
+				this.draw = this.drawNormal;
+			}
+		}
+	},
+
+	drawDeath: function(contexto){
+		contexto.drawImage(spriteSheet, 163 + (this.explosionIndex * 16), 79, 16, 15, this.x, this.y, 16, 15);
+	},
+
+	updateNormal: function(){
+		this.y++;
+
+		if(this.y > 480){
+			this.y = -20;
+			this.x = (Math.random() * 400) + 40;
+		}
+	},
+
+	drawNormal: function(contexto){
+		contexto.drawImage(spriteSheet, 5, 200, 14, 15, this.x, this.y, 14, 15);
 	},
 
 	update: function(){
