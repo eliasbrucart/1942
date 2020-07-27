466870e313b8a5e8aef12ac867c715bb1effa48e
Elias <eliasbrucart@hardgames.com.ar>
2020-07-26 22:27:04 -0300

    Added background animation

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,12 @@
 var backgroundGame = {
+	y: 0,
 	update: function(delta){
-
+		this.y++;
+		this.y = this.y % 480;
 	},
 
 	draw: function(contexto){
-		contexto.drawImage(background, 0, 0);
+		contexto.drawImage(background, 0, this.y - 480);
+		contexto.drawImage(background, 0, this.y);
 	}
 };
