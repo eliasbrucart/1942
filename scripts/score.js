87a1bac659f757551dd0c43f497cb6ad4d13a678
Elias <eliasbrucart@hardgames.com.ar>
2020-08-07 22:39:16 -0300

    Added high Score

--- scripts/score.js
+++ scripts/score.js
@@ -1,6 +1,9 @@
 var score = {
 	update: function(){
-
+		if(points > lastPoints){
+			localStorage['points'] = points;
+			lastPoints = points;
+		}
 	},
 
 	draw: function(contexto){
@@ -12,8 +15,13 @@ var score = {
 		degree.addColorStop(0, "rgb(170, 119, 68)");
 		degree.addColorStop(1, "white");
 		contexto.fillStyle = degree;
+
 		contexto.strokeText(points, 10, 40);
 		contexto.fillText(points, 9, 39);
+
+		contexto.strokeText('HI:' + lastPoints, 300, 40);
+		contexto.fillText('HI:' + lastPoints, 299, 39);
+		
 		contexto.restore();
 	}
 };
