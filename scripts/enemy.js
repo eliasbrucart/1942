6ccc443bd498db4f4e07d55360318f2e8f420ad1
Elias <eliasbrucart@hardgames.com.ar>
2020-08-12 21:38:14 -0300

    Modified enemy object for jsGFwk

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -1,13 +1,22 @@
 var enemy = {
 	x:(Math.random() * 400) + 40,
 	y:1,
+	width: 14,
+	height: 15,
 	name: 'enemy',
 	explosionIndex: 0,
 	explosionAccumulator: 0,
 
+	onInit: function(parameters){
+		this.x = parameters.x;
+		this.y = parameters.y;
+		this.onUpdate = this.updateNormal;
+		this.onDraw = this.drawNormal;
+	},
+
 	impact: function(){
-		this.update = this.updateDeath;
-		this.draw = this.drawDeath;
+		this.onUpdate = this.updateDeath;
+		this.onDraw = this.drawDeath;
 	},
 
 	updateDeath: function(delta){
@@ -17,42 +26,37 @@ var enemy = {
 			this.explosionIndex++;
 
 			if(this.explosionIndex >= 3){
-				this.explosionIndex = 0;
+				this.destroy();
+				/*this.explosionIndex = 0;
 				this.y = -20;
 				this.x = (Math.random() * 400) + 40;
 				this.update = this.updateNormal;
-				this.draw = this.drawNormal;
+				this.draw = this.drawNormal;*/
 			}
 		}
 	},
 
 	drawDeath: function(contexto){
-		contexto.drawImage(spriteSheet, 163 + (this.explosionIndex * 16), 79, 16, 15, this.x, this.y, 16, 15);
+		contexto.drawImage(jsGFwk.ResourceManager.graphics.main.image, 163 + (this.explosionIndex * 16), 79, 16, 15, this.x, this.y, 16, 15);
 	},
 
 	updateNormal: function(){
-		this.y++;
+		this.y += 2;
 
 		if(this.y > 480){
-			this.y = -20;
-			this.x = (Math.random() * 400) + 40;
+			this.destroy();
+			/*this.y = -20;
+			this.x = (Math.random() * 400) + 40;*/
 		}
 	},
 
 	drawNormal: function(contexto){
-		contexto.drawImage(spriteSheet, 5, 200, 14, 15, this.x, this.y, 14, 15);
+		contexto.drawImage(jsGFwk.ResourceManager.graphics.main.image, 5, 200, 14, 15, this.x, this.y, 14, 15);
 	},
 
-	update: function(){
-		this.y++;
-
-		if(this.y > 480){
-			this.y = -20;
-			this.x = (Math.random() * 400) + 40;
-		}
+	onUpdate: function(){
 	},
 
-	draw: function(contexto){
-		contexto.drawImage(spriteSheet, 5, 200, 14, 15, this.x, this.y, 14, 15);
+	onDraw: function(contexto){
 	}
 };
