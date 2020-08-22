1b64853ee7a62bb786fb4e4871b25ce89be62765
Elias <eliasbrucart@hardgames.com.ar>
2020-08-22 03:45:05 -0300

    Boot set as game state

--- Phaser/index.php
+++ Phaser/index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>1942 Phaser</title>
 	<script src="phaser/phaser.min.js" type="application/javascript"></script>
+	<script src="scripts/boot.js" type="application/javascript"></script>
 </head>
 <body>
 	<script type="application/javascript">
@@ -11,6 +12,10 @@
 
 		window.onload = function(){
 			game = new Phaser.Game(640, 480, Phaser.AUTO, '');
+
+			game.state.add('boot', boot);
+
+			game.state.start('boot');
 		};
 	</script>
 </body>
