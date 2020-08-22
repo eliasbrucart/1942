a4928368ce7d99237905f39cd047ab271ce5c744
Elias <eliasbrucart@hardgames.com.ar>
2020-08-22 03:12:23 -0300

    Window initialized with phaser

--- Phaser/index.php
+++ Phaser/index.php
@@ -3,8 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<title>1942 Phaser</title>
+	<script src="phaser/phaser.min.js" type="application/javascript"></script>
 </head>
 <body>
-	
+	<script type="application/javascript">
+		var game = undefined;
+
+		window.onload = function(){
+			game = new Phaser.Game(640, 480, Phaser.AUTO, '');
+		};
+	</script>
 </body>
 </html>
