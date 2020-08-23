a5aa1f61321d98e96df8df8d30ba003e7e8c7d85
Elias <eliasbrucart@hardgames.com.ar>
2020-08-23 03:09:29 -0300

    Enhanced infinite scroll

--- Phaser/scripts/hud.js
+++ Phaser/scripts/hud.js
@@ -10,14 +10,19 @@ var hud = {
 		game.stage.backgroundColor = 'ffffff';
 
 		this.background = game.add.sprite(0, 0, 'background');
-		this.background = game.add.sprite(0, 0, 'background2');
+		this.background2 = game.add.sprite(0, 0, 'background2');
+
+		this.background3 = game.add.sprite(0, 0, 'background');
+		this.background4 = game.add.sprite(0, 0, 'background2');
 	},
 
 	update: function update(){
 		this.background.y += this.backgroundSpeed;
+		this.background3.y = this.background.y - 800;
 		this.background.y = this.backgroundSpeed.y % 800;
 
 		this.background2.y += this.background2Speed;
+		this.background4.y = this.background4.y - 800;
 		this.background2.y = this.background2Speed.y % 800;
 	}
 };
